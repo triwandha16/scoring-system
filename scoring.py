c607a4e1a62ca01a2f1bf12aa3189e3f50f8a587
triw <triwandha@gmail.com>
2017-02-07 07:57:21 +0000

    add credit

--- scoring.py
+++ scoring.py
@@ -6,6 +6,7 @@ pygame.init()
 def OpeningText():
 	print "***********************"
 	print "**  SCORING SYSTEM   **"
+	print "**   triwandha16     **"
 	print "***********************"
 	pygame.joystick.init()
 	joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
