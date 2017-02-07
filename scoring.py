6df5ff3a18a54aae7732f774f69074c77f30b3a6
triw <triwandha@gmail.com>
2017-02-07 07:09:23 +0000

    Edit, nambahin email.

--- scoring.py
+++ scoring.py
@@ -6,6 +6,8 @@ pygame.init()
 def OpeningText():
 	print "***********************"
 	print "**  SCORING SYSTEM   **"
+	print "**-------------------**"
+	print "**triwandha@gmail.com**"
 	print "***********************"
 	pygame.joystick.init()
 	joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
